23f65c8ebbf407501a02c2c7f9f28a137e54096f
Ildar Sagdejev <specious@gmail.com>
2021-08-06 15:36:27 -0400

    Updated dependencies and switched to maxogden/commonjs-html-prettyprinter

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var clone = require('clone');
 module.exports = function(options){
   var opts = options ? clone(options) : {};
   return es.map(function(file, cb){
-  	file.contents = new Buffer(html.prettyPrint(String(file.contents), opts));
-  	cb(null, file);
+    file.contents = new Buffer(html.prettyPrint(String(file.contents), opts));
+    cb(null, file);
   });
 }
